b78ecc9efd232fa668dc970abfb35c38797d612b
Mistesq <mistesqr@gmail.com>
2020-07-01 18:00:56 +0300

    add components ActiveQuiz, AnswerList, AnswerItem

--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -1,15 +1,38 @@
 import React, {Component} from 'react';
 import classes from './Quiz.module.scss';
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
 
 class Quiz extends Component {
   state = {
-    quiz: []
+    quiz: [
+      {
+        question: 'What color is the sky?',
+        rightAnswerId: 4,
+        answers: [
+          {text: 'green', id: 1},
+          {text: 'yellow', id: 2},
+          {text: 'red', id: 3},
+          {text: 'blue', id: 4}
+        ]
+      }
+    ]
+  }
+
+  onAnswerClickHandler = (answerId) => {
+    console.log('Answer id: ', answerId);
   }
 
   render() {
     return (
       <div className={classes.Quiz}>
-        <h1>Quiz</h1>
+        <div className={classes.QuizWrapper}>
+          <h1>Answer all questions</h1>
+          <ActiveQuiz
+            answers={this.state.quiz[0].answers}
+            question={this.state.quiz[0].question}
+            onAnswerClick={this.onAnswerClickHandler}
+          />
+        </div>
       </div>
     );
   }
