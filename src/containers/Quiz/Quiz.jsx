4b4dc688f7c7d209f029686ec5d58c1d4fa12734
Mistesq <mistesqr@gmail.com>
2020-07-02 11:14:51 +0300

    logic change question

--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -4,22 +4,37 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
 
 class Quiz extends Component {
   state = {
+    activeQuestion: 0,
     quiz: [
       {
         question: 'What color is the sky?',
         rightAnswerId: 4,
+        id: 1,
         answers: [
           {text: 'green', id: 1},
           {text: 'yellow', id: 2},
           {text: 'red', id: 3},
           {text: 'blue', id: 4}
         ]
+      },
+      {
+        question: 'What year was Leonardo da Vinci born?',
+        rightAnswerId: 2,
+        id: 2,
+        answers: [
+          {text: '1343', id: 1},
+          {text: '1452', id: 2},
+          {text: '1401', id: 3},
+          {text: '1487', id: 4}
+        ]
       }
     ]
   }
 
   onAnswerClickHandler = (answerId) => {
-    console.log('Answer id: ', answerId);
+    this.setState({
+      activeQuestion: this.state.activeQuestion + 1
+    });
   }
 
   render() {
@@ -28,9 +43,11 @@ class Quiz extends Component {
         <div className={classes.QuizWrapper}>
           <h1>Answer all questions</h1>
           <ActiveQuiz
-            answers={this.state.quiz[0].answers}
-            question={this.state.quiz[0].question}
+            answers={this.state.quiz[this.state.activeQuestion].answers}
+            question={this.state.quiz[this.state.activeQuestion].question}
             onAnswerClick={this.onAnswerClickHandler}
+            quizLenght={this.state.quiz.length}
+            answerNumber={this.state.activeQuestion + 1}
           />
         </div>
       </div>
