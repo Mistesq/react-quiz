445cfcad65d7ced904ff75ea0d0a7c3b382464b7
Mistesqr <mistesqr@gmail.com>
2020-09-17 19:43:44 +0300

    add select component

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import classes from './QuizCreator.module.scss';
 import Button from '../../components/UI/Button/Button';
-import {createControl} from '../../form/formFramework';
 import Input from '../../components/UI/Input/Input';
+import Select from '../../components/UI/Select/Select';
+import {createControl} from '../../form/formFramework';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 
 function createOptionControl(number) {
@@ -30,6 +31,7 @@ class QuizCreator extends Component {
 
     state = {
         quiz: [],
+        rightAnswerId: 1,
         formControls: createFormControls()
     }
 
@@ -90,7 +92,26 @@ class QuizCreator extends Component {
         })
     }
 
+    selectChangeHandler = event => {
+        this.setState({
+            rightAnswerId: +event.target.value
+        });
+    }
+
     render() {
+
+        const select = <Select
+            label="Choose the correct answer"
+            value={this.state.rightAnswerId}
+            onChange={this.selectChangeHandler}
+            options={[
+                {text: 1, value: 1},
+                {text: 2, value: 2},
+                {text: 3, value: 3},
+                {text: 4, value: 4}
+            ]}
+        />;
+
         return (
             <div className={classes.QuizCreator}>
                 <div>
@@ -100,7 +121,8 @@ class QuizCreator extends Component {
 
                         { this.renderControls() }
 
-                        <select></select>
+                        { select }
+
                         <Button type="primary" onClick={this.addQuestionHandler}>Add Question</Button>
                         <Button type="success" onClick={this.createQuizHandler}>Create Quiz</Button>
                     </form>
